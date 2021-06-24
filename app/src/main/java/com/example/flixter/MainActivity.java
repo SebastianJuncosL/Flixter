94e95970d8201810d30485b2deafb9cec1cb7b5d
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-06-24 09:33:21 -0500

    Added json manipulation and movies are now in a Java List

--- app/src/main/java/com/example/flixter/MainActivity.java
+++ app/src/main/java/com/example/flixter/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.flixter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+// Import http client -> downloaded in the build.gradle file
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixter.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+    // make a GET request to the url for currently playing movies
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=fcb22cc7e926955acc46ed9a94f553b0"; // Given that the key is inside the url, we don't need to add parameters to the AsyncHttpClient
+    public static final String TAG = "MainActivity"; // to easily log data
 
+    List<Movie> movies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Create an instance fo the http client
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject; // get json from request
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results"); // Convert json object to json string
+                    Log.i(TAG, "Results: " + results.toString()); // Log results as a string if successful
+                    movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
     }
 }
